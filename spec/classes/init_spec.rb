b737be3d44a26a074afbcd07d8562d5a9a52ebc6
p0bailey <phillip@bailey.st>
2014-10-26 21:17:16 +0000

    comment amend

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 describe 'puppet_rspec_hello_adv1' do
 
-  context 'Test whether apache package and service are installed and running.' do
+  context 'Test whether class puppet_rspec_hello_adv1 exists.' do
     it { should contain_class('puppet_rspec_hello_adv1') }
+    end
+  context 'Test whether the module can compile.' do
+    it { should compile }
   end
 
+  context 'Test whether apache package and service are installed and running.' do
   it {
       should contain_package('apache2').with( { 'name' => 'apache2' } )
       should contain_service('apache2').with( {
@@ -12,5 +16,6 @@ describe 'puppet_rspec_hello_adv1' do
         'ensure' => 'running',
         'enable' => 'true' } )
     }
+  end
 
 end
