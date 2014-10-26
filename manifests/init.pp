bf48ffb5d2410365da36c6280c00055463438bce
p0bailey <phillip@bailey.st>
2014-10-26 14:52:49 +0000

    first test

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,5 +37,12 @@
 #
 class puppet_rspec_hello_adv1 {
 
+package { 'apache2':
+    ensure => 'installed'
+}
+
+service { 'apache2':
+  ensure => 'running',
+}
 
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -4,4 +4,10 @@ describe 'puppet_rspec_hello_adv1' do
   context 'with defaults for all parameters' do
     it { should contain_class('puppet_rspec_hello_adv1') }
   end
+
+  it {
+      should contain_package('apache2').with( { 'name' => 'apache2' } )
+      should contain_service('apache2').with( { 'name' => 'apache2' } )
+    }
+
 end
